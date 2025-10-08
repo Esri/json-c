cbba9171ace28b80587bc5c6f9d5d8ca60731968
Joshua Weenig <jweenig@esri.com>
2025-10-08 12:49:44 -0700

    tmp: Fix if-def indents
    Since there area couple levels of if-defs, want to make it easier
    on my eyes to follow.

--- src_generated/port/config.h
+++ src_generated/port/config.h
@@ -2,21 +2,21 @@
 #define _json_config_h_
 
 #if defined(RTC_COCOA_FAMILY)
-#include "config_macos.h"
+    #include "config_macos.h"
 #elif defined(RTC_LINUX_DESKTOP)
-#include "config_linux.h"
+    #include "config_linux.h"
 #elif defined(RTC_LINUX_ANDROID)
     #if defined(RTC_X64) || defined(RTC_ARM64)
-    #include "config_android_arm64_x64.h"
+        #include "config_android_arm64_x64.h"
     #elif defined(RTC_X86) || defined(RTC_ARMV7)
-    #include "config_android_armv7_x86.h"
+        #include "config_android_armv7_x86.h"
     #else
-    #error "Unable to include json-c config.h for Android"
+        #error "Unable to include json-c config.h for Android"
     #endif
 #elif defined(RTC_WINDOWS_FAMILY)
-#include "config_windows.h"
+    #include "config_windows.h"
 #else
-#error "Unable to include json-c config.h"
+    #error "Unable to include json-c config.h"
 #endif
 
 #endif /* _json_config_h_ */
